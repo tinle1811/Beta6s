398f8b1b5378f4576c6ab3a7b9fa8382af55f216
V1OLE123 <longnguyenevil@gmail.com>
2024-12-23 17:59:00 +0700

    Khánh: PhanTrang

--- app/Models/SanPham.php
+++ app/Models/SanPham.php
@@ -18,5 +18,114 @@ class SanPham extends Model
         'LoaiSP',
         'TrangThai',
     ];
-    
+
+    public function getProductId()
+    {
+        return $this->attributes['MaSP'];
+    }
+
+    public function setProductId($id)
+    {
+        $this->attributes['MaSP'] = $id;
+    }
+
+    public function getProductName()
+    {
+        return $this->attributes['TenSP'];
+    }
+
+    public function setProductName($name)
+    {
+        $this->attributes['TenSP'] = $name;
+    }
+
+    public function getProductPrice()
+    {
+        return $this->attributes['Gia'];
+    }
+
+    public function setProductPrice($price)
+    {
+        $this->attributes['Gia'] = $price;
+    }
+
+    public function getProductQuantity()
+    {
+        return $this->attributes['SoLuong'];
+    }
+
+    public function setProductQuantity($quantity)
+    {
+        $this->attributes['SoLuong'] = $quantity;
+    }
+
+    public function getProductDescription()
+    {
+        return $this->attributes['MoTa'];
+    }
+
+    public function setProductDescription($description)
+    {
+        $this->attributes['MoTa'] = $description;
+    }
+
+    public function getProductSlug()
+    {
+        return $this->attributes['Slug'];
+    }
+
+    public function setProductSlug($slug)
+    {
+        $this->attributes['Slug'] = $slug;
+    }
+
+    public function getProductImage()
+    {
+        return $this->attributes['HinhAnh'];
+    }
+
+    public function setProductImage($image)
+    {
+        $this->attributes['HinhAnh'] = $image;
+    }
+
+    public function getProductType()
+    {
+        return $this->attributes['LoaiSP'];
+    }
+
+    public function setProductType($producttype)
+    {
+        $this->attributes['LoaiSP'] = $producttype;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->attributes['created_at'];
+    }
+
+    public function setCreatedAt($createdAt)
+    {
+        $this->attributes['created_at'] = $createdAt;
+    }
+
+    public function getUpdatedAt()
+    {
+        return $this->attributes['updated_at'];
+    }
+
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->attributes['updated_at'] = $updatedAt;
+    }
+
+    public function getDeletedAt()
+    {
+        return $this->attributes['deleted_at'];
+    }
+
+    public function setDeletedAt($deletedAt)
+    {
+        $this->attributes['deleted_at'] = $deletedAt;
+    }
 }
