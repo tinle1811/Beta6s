8cf3b8ba9fe8df6bcee25612fd6526bc8814f002
Thanh Trúc <0306221288@caothang.edu.vn>
2025-01-03 22:30:48 +0700

    Truc: Chỉnh sửa dịch vụ và vận chuyển

--- resources/views/user/layouts/home/shippingArea.blade.php
+++ resources/views/user/layouts/home/shippingArea.blade.php
@@ -8,8 +8,8 @@
                             <img src="assets/img/about/shipping1.png" alt="">
                         </div>
                         <div class="shipping_content">
-                            <h2>Free Shipping</h2>
-                            <p>Free shipping on all US order</p>
+                            <h2>Miễn phí vận chuyển</h2>
+                            <p>Miễn phí vận chuyển cho tất cả các đơn hàng tại Việt Nam</p>
                         </div>
                     </div>
                 </div>
@@ -19,19 +19,19 @@
                             <img src="assets/img/about/shipping2.png" alt="">
                         </div>
                         <div class="shipping_content">
-                            <h2>Support 24/7</h2>
-                            <p>Contact us 24 hours a day</p>
+                            <h2>Hỗ trợ 24/7</h2>
+                            <p>Liên hệ với chúng tôi 24 giờ một ngày</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6">
                     <div class="single_shipping">
                         <div class="shipping_icone">
-                            <img src="assets/img/about/shipping3.png" alt="">
+                            <img src="assets/img/about/shipping3.png" alt="" style="width: 48px; height: 45px;">
                         </div>
                         <div class="shipping_content">
-                            <h2>100% Money Back</h2>
-                            <p>You have 30 days to Return</p>
+                            <h2>100% sản phẩm chính hãng</h2>
+                            <p>Sản phẩm chính hãng từ Medicom Toy</p>
                         </div>
                     </div>
                 </div>
@@ -41,8 +41,8 @@
                             <img src="assets/img/about/shipping4.png" alt="">
                         </div>
                         <div class="shipping_content">
-                            <h2>Payment Secure</h2>
-                            <p>We ensure secure payment</p>
+                            <h2>Thanh toán an toàn</h2>
+                            <p>Chúng tôi đảm bảo thanh toán an toàn</p>
                         </div>
                     </div>
                 </div>
