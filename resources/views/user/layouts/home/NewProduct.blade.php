7ce9e118e858b5c48a529862fae3c59b1f2b2306
Khoa Nguyễn <0306221240@caothang.edu.vn>
2025-01-04 16:07:12 +0700

    Khoa đã hoàn thành hiển thị danh sách sản phẩm mới.

---
+++ resources/views/user/layouts/home/NewProduct.blade.php
@@ -0,0 +1,54 @@
+        <!--product area start-->
+        <section class="product_area mb-46">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="section_title">
+                            <h2>Sản Phẩm Mới</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="product_carousel product_column5 owl-carousel">
+                    @foreach ($viewData["DSSP-Moi"] as $product)
+                        <article class="single_product">
+                            <figure>
+                                <div class="product_thumb">
+                                    <a class="primary_img" href="{{route('user.home.show')}}"><img src="{{ asset('/assetsUser/img_product'.$product->getProductImage()) }}"
+                                            alt="{{ $product->getProductName() }}"></a>
+                                    {{-- <a class="secondary_img" href="{{route('user.home.show')}}"><img
+                                            src="assets/img/product/product2.jpg" alt=""></a> --}}
+                                    {{-- <div class="label_product">
+                                        <span class="label_sale">sale</span>
+                                    </div> --}}
+                                    <div class="action_links">
+                                        <ul>
+                                            <li class="wishlist"><a href="wishlist.html" title="Add to Wishlist"><i
+                                                        class="fa fa-heart-o" aria-hidden="true"></i></a></li>
+                                            <li class="compare"><a href="#" title="compare"><span class="ion-levels"></span></a>
+                                            </li>
+                                            <li class="quick_button"><a href="#" data-bs-toggle="modal" data-bs-target="#modal_box"
+                                                    title="quick view"> <span class="ion-ios-search-strong"></span></a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="add_to_cart">
+                                        <a href="cart.html" title="add to cart">Thêm vào giỏ hàng</a>
+                                    </div>
+                                    {{-- <div class="product_timing">
+                                        <div data-countdown="2023/12/15"></div>
+                                    </div> --}}
+                                </div>
+                                <figcaption class="product_content">
+                                    <div class="price_box">
+                                        {{-- <span class="old_price">$86.00</span> --}}
+                                        <span class="current_price">{{ $product->getProductPrice() }}</span>
+                                    </div>
+                                    <h3 class="product_name"><a href="{{route('user.home.show')}}">{{ Str::limit($product->getProductName(), 20, '...') }}</a></h3>
+                                    <p class="product_description" style="font-size: .7rem">{{ Str::limit($product->getProductDescription(), 60, '...') }}</p>
+                                </figcaption>
+                            </figure>
+                        </article>
+                    @endforeach
+                </div>
+            </div>
+        </section>
+        <!--product area end-->
