73d2b2ff122f888cf9776b4b4ceb5025a4dfb187
Khoa Nguyễn <0306221240@caothang.edu.vn>
2025-01-04 16:02:15 +0700

    Khoa đã chỉnh sửa HomeController để truy vấn dữ liệu.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\DB;
+
+use Carbon\Carbon;
+
 use App\Models\SanPham;
 
 class HomeController extends Controller
@@ -13,8 +17,20 @@ class HomeController extends Controller
     public function index()
     {
         $viewData['title'] = "Trang chủ";
-        $viewData["DSSP-BanChay"] = SanPham::limit(10)->get();
-        $viewData["DSSP-NoiBat"] = SanPham::skip(10)->limit(10)->get();
+        $viewData["DSSP-Slider"] = SanPham::limit(3)->get();
+        $viewData["DSSP-BanChay"] = SanPham::join('chi_tiet_hoa_dons', 'san_phams.MaSP', '=', 'chi_tiet_hoa_dons.MaSP')
+                                    ->join('hoa_dons', 'chi_tiet_hoa_dons.MaHD', '=', 'hoa_dons.MaHD')
+                                    ->select('san_phams.HinhAnh', 'san_phams.Gia', 'san_phams.TenSP', 'san_phams.MoTa', DB::raw('SUM(chi_tiet_hoa_dons.SoLuong) as total_quantity'))
+                                    ->where('hoa_dons.TrangThai', '1')
+                                    ->groupBy('san_phams.HinhAnh', 'san_phams.Gia', 'san_phams.TenSP', 'san_phams.MoTa')
+                                    ->orderBy('total_quantity', 'Desc')
+                                    ->limit(10)
+                                    ->get();
+        $viewData["DSSP-NoiBat"] = SanPham::where('TrangThai',2)->get();
+        $viewData["DSSP-Moi"] = SanPham::where('created_at', '>=', Carbon::now()->subDays(40))
+                                ->orderBy('created_at', 'Desc')
+                                ->limit(10)
+                                ->get();
         return view('user.home.index')->with('viewData',$viewData);
     }
 
