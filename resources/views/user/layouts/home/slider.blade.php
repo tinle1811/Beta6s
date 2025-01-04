6bbc06254cc1eddb0180a4d8b7b8d3795f8d7dc3
Khoa Nguyễn <0306221240@caothang.edu.vn>
2025-01-04 16:07:47 +0700

    Khoa đã hoàn thành hiển thị sản phẩm trên slider.

--- resources/views/user/layouts/home/slider.blade.php
+++ resources/views/user/layouts/home/slider.blade.php
@@ -1,22 +1,25 @@
     <!--slider area start-->
     <section class="slider_section mb-70">
         <div class="slider_area owl-carousel">
-            <div class="single_slider d-flex align-items-center" data-bgimg="assets/img/slider/slider1.jpg">
+            @foreach ($viewData['DSSP-Slider'] as $product)
+                <div class="single_slider d-flex align-items-center" data-bgimg="{{ asset('/assetsUser/img_product/'.$product->getProductImage()) }}" style="background-repeat: no-repeat; background-position: right center; background-size:  800px 500px;">
                 <div class="container">
                     <div class="row">
                         <div class="col-12">
                             <div class="slider_content">
-                                <h1> best skin</h1>
-                                <h2>qled 75 inch q7f</h2>
-                                <p>exclusive offer <span> 20% off </span> this week</p>
-                                <a class="button" href="shop.html">shopping now</a>
+                                <h1> Gấu Bearbrick</h1>
+                                <h2>{{ Str::limit($product->getProductName(), 15, '...') }}</h2>
+                                {{-- <p>exclusive offer <span> 20% off </span> this week</p> --}}
+                                <p>{{ Str::limit($product->getProductDescription(), 60, '...') }}</p>
+                                <a class="button" href="shop.html">Mua sắm ngay</a>
                             </div>
                         </div>
                     </div>
                 </div>
 
             </div>
-            <div class="single_slider d-flex align-items-center" data-bgimg="assets/img/slider/slider2.jpg">
+            @endforeach
+            {{-- <div class="single_slider d-flex align-items-center" data-bgimg="assets/img/slider/slider2.jpg">
                 <div class="container">
                     <div class="row">
                         <div class="col-12">
@@ -43,7 +46,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> --}}
         </div>
     </section>
     <!--slider area end-->
