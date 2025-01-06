dfedca4f9e69142de413f855b64840132dbabfb9
Trung Tín <90820551+tinle1811@users.noreply.github.com>
2025-01-07 00:46:32 +0700

    Tín: Hoàn thành popup đăng nhập và thêm sản phẩm vào giỏ hàng

--- resources/views/user/layouts/home/featuredProduct.blade.php
+++ resources/views/user/layouts/home/featuredProduct.blade.php
@@ -1,209 +1,51 @@
-        <!--featured product area start-->
-        <section class="featured_product_area mb-70">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="section_title">
-                            <h2>Sản phẩm nổi bật</h2>
-                        </div>
-                    </div>
-                </div>
-                <div class="row featured_container featured_column3">
-                    <div class="col-lg-4">
-                        <article class="single_product">
-                            <figure>
-                                <div class="product_thumb">
-                                    <a class="primary_img" href="{{route('user.home.show')}}"><img
-                                            src="assets/img/product/product13.jpg" alt=""></a>
-                                    <a class="secondary_img" href="{{route('user.home.show')}}"><img
-                                            src="assets/img/product/product14.jpg" alt=""></a>
-                                    <div class="label_product">
-                                        <span class="label_sale">sale</span>
-                                    </div>
-                                </div>
-                                <figcaption class="product_content">
-                                    <div class="price_box">
-                                        <span class="current_price">$79.00</span>
-                                    </div>
-                                    <h3 class="product_name"><a href="{{route('user.home.show')}}">Pellentesque  posuere hendrerit
-                                            dui</a></h3>
-                                    <div class="add_to_cart">
-                                        <a href="javascript:void(0);"  onclick="addToCart()" title="add to cart">Add to cart</a>
-                                    </div>
-                                </figcaption>
-                            </figure>
-                        </article>
-                    </div>
-                    <div class="col-lg-4">
-                        <article class="single_product">
-                            <figure>
-                                <div class="product_thumb">
-                                    <a class="primary_img" href="{{route('user.home.show')}}"><img
-                                            src="assets/img/product/product15.jpg" alt=""></a>
-                                    <a class="secondary_img" href="{{route('user.home.show')}}"><img
-                                            src="assets/img/product/product16.jpg" alt=""></a>
-                                </div>
-                                <figcaption class="product_content">
-                                    <div class="price_box">
-                                        <span class="old_price">$86.00</span>
-                                        <span class="current_price">$79.00</span>
-                                    </div>
-                                    <h3 class="product_name"><a href="{{route('user.home.show')}}">Auctor gravida enim pellentesque
-                                            quam</a></h3>
-                                    <div class="add_to_cart">
-                                        <a href="javascript:void(0);" onclick="addToCart()" title="add to cart">Add to cart</a>
-                                    </div>
-                                </figcaption>
-                            </figure>
-                        </article>
-                    </div>
-                    <div class="col-lg-4">
-                        <article class="single_product">
-                            <figure>
-                                <div class="product_thumb">
-                                    <a class="primary_img" href="{{route('user.home.show')}}"><img
-                                            src="assets/img/product/product17.jpg" alt=""></a>
-                                    <a class="secondary_img" href="{{route('user.home.show')}}"><img
-                                            src="assets/img/product/product18.jpg" alt=""></a>
-                                    <div class="label_product">
-                                        <span class="label_sale">sale</span>
-                                    </div>
-                                </div>
-                                <figcaption class="product_content">
-                                    <div class="price_box">
-                                        <span class="old_price">$86.00</span>
-                                        <span class="current_price">$79.00</span>
-                                    </div>
-                                    <h3 class="product_name"><a href="{{route('user.home.show')}}">Kaoreet lobortis sagittis
-                                            pellentesque</a></h3>
-                                    <div class="add_to_cart">
-                                        <a href="javascript:void(0);" onclick="addToCart()" title="add to cart">Add to cart</a>
-                                    </div>
-                                </figcaption>
-                            </figure>
-                        </article>
-                    </div>
-                    <div class="col-lg-4">
-                        <article class="single_product">
-                            <figure>
-                                <div class="product_thumb">
-                                    <a class="primary_img" href="{{route('user.home.show')}}"><img
-                                            src="assets/img/product/product19.jpg" alt=""></a>
-                                    <a class="secondary_img" href="{{route('user.home.show')}}"><img
-                                            src="assets/img/product/product20.jpg" alt=""></a>
-                                    <div class="label_product">
-                                        <span class="label_sale">sale</span>
-                                    </div>
-                                </div>
-                                <figcaption class="product_content">
-                                    <div class="price_box">
-                                        <span class="old_price">$86.00</span>
-                                        <span class="current_price">$79.00</span>
-                                    </div>
-                                    <h3 class="product_name"><a href="{{route('user.home.show')}}">Cras neque honcus consectetur
-                                            magna</a></h3>
-                                    <div class="add_to_cart">
-                                        <a href="javascript:void(0);" onclick="addToCart()" title="add to cart">Add to cart</a>
-                                    </div>
-                                </figcaption>
-                            </figure>
-                        </article>
-                    </div>
-                    <div class="col-lg-4">
-                        <article class="single_product">
-                            <figure>
-                                <div class="product_thumb">
-                                    <a class="primary_img" href="{{route('user.home.show')}}"><img
-                                            src="assets/img/product/product21.jpg" alt=""></a>
-                                    <a class="secondary_img" href="{{route('user.home.show')}}"><img
-                                            src="assets/img/product/product22.jpg" alt=""></a>
-                                </div>
-                                <figcaption class="product_content">
-                                    <div class="price_box">
-                                        <span class="current_price">$79.00</span>
-                                    </div>
-                                    <h3 class="product_name"><a href="{{route('user.home.show')}}">Duis pulvinar obortis eleifend
-                                            elementum</a></h3>
-                                    <div class="add_to_cart">
-                                        <a href="javascript:void(0);" onclick="addToCart()" title="add to cart">Add to cart</a>
-                                    </div>
-                                </figcaption>
-                            </figure>
-                        </article>
-                    </div>
-                    <div class="col-lg-4">
-                        <article class="single_product">
-                            <figure>
-                                <div class="product_thumb">
-                                    <a class="primary_img" href="{{route('user.home.show')}}"><img
-                                            src="assets/img/product/product23.jpg" alt=""></a>
-                                    <a class="secondary_img" href="{{route('user.home.show')}}"><img
-                                            src="assets/img/product/product24.jpg" alt=""></a>
-                                </div>
-                                <figcaption class="product_content">
-                                    <div class="price_box">
-                                        <span class="old_price">$86.00</span>
-                                        <span class="current_price">$79.00</span>
-                                    </div>
-                                    <h3 class="product_name"><a href="{{route('user.home.show')}}">Fusce ultricies dolor vitae
-                                            tristique suscipit</a></h3>
-                                    <div class="add_to_cart">
-                                        <a href="javascript:void(0);" onclick="addToCart()" title="add to cart">Add to cart</a>
-                                    </div>
-                                </figcaption>
-                            </figure>
-                        </article>
-                    </div>
-                    <div class="col-lg-4">
-                        <article class="single_product">
-                            <figure>
-                                <div class="product_thumb">
-                                    <a class="primary_img" href="{{route('user.home.show')}}"><img
-                                            src="assets/img/product/product15.jpg" alt=""></a>
-                                    <a class="secondary_img" href="{{route('user.home.show')}}"><img
-                                            src="assets/img/product/product16.jpg" alt=""></a>
-                                </div>
-                                <figcaption class="product_content">
-                                    <div class="price_box">
-                                        <span class="old_price">$86.00</span>
-                                        <span class="current_price">$79.00</span>
-                                    </div>
-                                    <h3 class="product_name"><a href="{{route('user.home.show')}}">Natus erro at congue massa
-                                            commodo sit</a></h3>
-                                    <div class="add_to_cart">
-                                        <a href="javascript:void(0);" onclick="addToCart()" title="add to cart">Add to cart</a>
-                                    </div>
-                                </figcaption>
-                            </figure>
-                        </article>
-                    </div>
-                    <div class="col-lg-4">
-                        <article class="single_product">
-                            <figure>
-                                <div class="product_thumb">
-                                    <a class="primary_img" href="{{route('user.home.show')}}"><img
-                                            src="assets/img/product/product17.jpg" alt=""></a>
-                                    <a class="secondary_img" href="{{route('user.home.show')}}"><img
-                                            src="assets/img/product/product18.jpg" alt=""></a>
-                                    <div class="label_product">
-                                        <span class="label_sale">sale</span>
-                                    </div>
-                                </div>
-                                <figcaption class="product_content">
-                                    <div class="price_box">
-                                        <span class="old_price">$86.00</span>
-                                        <span class="current_price">$79.00</span>
-                                    </div>
-                                    <h3 class="product_name"><a href="{{route('user.home.show')}}">Cras neque honcus consectetur
-                                            magna</a></h3>
-                                    <div class="add_to_cart">
-                                        <a href="javascript:void(0);" onclick="addToCart()" title="add to cart">Add to cart</a>
-                                    </div>
-                                </figcaption>
-                            </figure>
-                        </article>
-                    </div>
+<!--featured product area start-->
+<section class="featured_product_area mb-70">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="section_title">
+                    <h2>Sản phẩm nổi bật</h2>
                 </div>
             </div>
-        </section>
-        <!--featured product area end-->
+        </div>
+        <div class="row featured_container featured_column3">
+            @foreach ($viewData['DSSP-NoiBat'] as $product)
+                <div class="col-lg-4">
+                    <article class="single_product">
+                        <figure>
+                            <div class="product_thumb">
+                                <a class="primary_img" href="{{ route('user.home.show') }}"><img
+                                        src="{{ asset('/assetsUser/img_product/' . $product->getProductImage()) }}"
+                                        alt="{{ $product->getProductName() }}"></a>
+                                {{-- <a class="secondary_img" href="{{ route('user.home.show') }}"><img
+                                        src="assets/img/product/product14.jpg" alt=""></a> --}}
+                                {{-- <div class="label_product">
+                                    <span class="label_sale">sale</span>
+                                </div> --}}
+                            </div>
+                            <figcaption class="product_content">
+                                <div class="price_box">
+                                    <span class="current_price">{{ number_format($product->getProductPrice(), 0, ',', '.') }} đ</span>
+                                </div>
+                                <h3 class="product_name"><a
+                                        href="{{ route('user.home.show') }}">{{ $product->getProductName() }}</a></h3>
+                                <div class="add_to_cart">
+                                    <form action="{{route('user.cart.add')}}" method="POST" class="add-to-cart-form" id="add-to-cart-form">
+                                        @csrf
+                                        <input type="hidden" name="MaSP" value="{{ $product->MaSP }}">
+                                        <input type="number" name="soLuong" value="1" min="1" style="display: none;">
+                                        @auth
+                                            <button type="submit"  class="btn btn-primary">Thêm vào giỏ hàng</button>
+                                        @else
+                                            <a href="javascript:void(0)" onclick="openLoginPopup()"  class="btn btn-primary">Thêm vào giỏ hàng</a>
+                                        @endauth
+                                    </form>
+                                </div>
+                            </figcaption>
+                        </figure>
+                    </article>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</section>
--- resources/views/user/layouts/popupLogin.blade.php
+++ resources/views/user/layouts/popupLogin.blade.php
@@ -1,36 +1,81 @@
-<div class="popup-container popup-login">
-    <div id="boxes" class="popup-container-inner">
-        <div id="dialog" class="window">
-            <div id="popup-close">
-                <span>X</span>
+<!-- Popup -->
+<!-- Popup -->
+<div id="login-popup" class="modal fade" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header text-center">
+                <h3 class="modal-title w-100">Đăng Nhập</h3>
+                <button type="button" class="btn-close" onclick="closeLoginPopup()" aria-label="Close"></button>
             </div>
-            <div class="box">
-                <div class="popup-title">
-                    <h2>Đăng nhập</h2>
+            <div class="modal-body d-flex justify-content-between pt-0">
+                <div  class="d-flex align-items-center">
+                    <img id="logo" src="{{asset('assets/img/logoBeta6s.jpg')}}"  class="">
                 </div>
-                <div class="popup-content">
-                    <form action="#" method="POST">
+                <div class="form-section" style="flex: 1; padding: 1.5rem;">
+                    <form action="{{ route('login') }}" method="POST">
                         @csrf
-                        <p>
-                            <label>Email: </label>
-                            <input type="email" name="email" required placeholder="Nhập địa chỉ email tại đây">
-                        </p>
-                        <p>
-                            <label>Mật Khẩu: </label>
-                            <input type="password" name="password" required placeholder="Nhập mật khẩu tại đây">
-                        </p>
-                        <div class="login_submit">
-                            <a href="#">Quên mật khẩu</a>
-                            <label for="remember">
-                                <input id="remember" type="checkbox"> Nhớ mật khẩu
-                            </label>
-                            <div class="submit-button">
-                                <button type="submit">Đăng nhập</button>
-                            </div>
+                        <div class="mb-3">
+                            <label for="Email" class="form-label">Email</label>
+                            <input type="email" id="Email" name="Email" class="form-control" placeholder="Nhập email"
+                             required>
                         </div>
+                        <div class="mb-3">
+                            <label for="Password" class="form-label">Mật khẩu</label>
+                            <input type="password" id="Password" name="Password" class="form-control"
+                                placeholder="Nhập mật khẩu" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary w-100">Đăng Nhập</button>
                     </form>
+                    <a href="#">Quên mật khẩu?</a>
+                    <div class="register_link text-center mt-1">
+                        <p>Bạn chưa có tài khoản?<a href="{{route("user.auth.register")}}" class="text-primary text-decoration-none">Đăng ký</a></p>
+                    </div>
                 </div>
+                
             </div>
         </div>
     </div>
 </div>
+
+<!-- Overlay -->
+<div id="overlay" style="display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background: rgba(0, 0, 0, 0.5); z-index: 999;" onclick="closeLoginPopup()"></div>
+{{-- <script>
+    function openLoginPopup() {
+    const loginModal = new bootstrap.Modal(document.getElementById('login-popup'));
+    loginModal.show();
+    }
+
+    function closeLoginPopup() {
+        const loginModal = bootstrap.Modal.getInstance(document.getElementById('login-popup'));
+        loginModal.hide();
+    }
+
+    // Xử lý gửi form
+    document.getElementById('login-form').addEventListener('submit', async function (e) {
+        e.preventDefault(); // Ngăn chặn reload trang
+
+        const formData = new FormData(this);
+
+        try {
+            const response = await fetch("{{ route('login') }}", {
+                method: 'POST',
+                headers: {
+                    'X-CSRF-TOKEN': document.querySelector('input[name="_token"]').value
+                },
+                body: formData
+            });
+
+            const result = await response.json();
+
+            if (response.ok) {
+                alert(result.message);
+                closeLoginPopup();
+                window.location.reload(); // Reload trang nếu cần
+            } else {
+                alert(result.message);
+            }
+        } catch (error) {
+            alert('Đã xảy ra lỗi! Vui lòng thử lại.');
+        }
+    });
+</script> --}}
