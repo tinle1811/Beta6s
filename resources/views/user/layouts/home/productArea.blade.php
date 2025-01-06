9c6cb0f8b7f4cca914f3e28aa3c65c809046aa6d
khánh nguyễn <khanhdkid123@gmail.com>
2025-01-06 08:36:15 +0700

    khánh: cập nhật mục sản phẩm bán chạy

--- resources/views/user/layouts/home/productArea.blade.php
+++ resources/views/user/layouts/home/productArea.blade.php
@@ -10,42 +10,54 @@
                 </div>
                 <div class="product_carousel product_column5 owl-carousel">
                     @foreach ($viewData["DSSP-BanChay"] as $product)
-                        <article class="single_product">
-                            <figure>
-                                <div class="product_thumb">
-                                    <a class="primary_img" href="{{route('user.home.show')}}"><img src="{{ asset('/assetsUser/img_product/' . $product->getProductImage()) }}"
-                                            alt="{{ $product->getProductName() }}"></a>
-                                    {{-- <a class="secondary_img" href="{{route('user.home.show')}}"><img
-                                            src="assets/img/product/product2.jpg" alt=""></a> --}}
-                                    {{-- <div class="label_product">
+                    <article class="single_product">
+                        <figure>
+                            <div class="product_thumb">
+                                <a class="primary_img" href="{{route('user.home.show')}}"><img
+                                        src="{{ asset('/assetsUser/img_product/' . $product->getProductImage()) }}"
+                                        alt="{{ $product->getProductName() }}"></a>
+                                {{-- <a class="secondary_img" href="{{route('user.home.show')}}"><img
+                                    src="assets/img/product/product2.jpg" alt=""></a> --}}
+                                {{-- <div class="label_product">
                                         <span class="label_sale">sale</span>
                                     </div> --}}
-                                    <div class="action_links">
-                                        <ul>
-                                            <li class="wishlist"><a href="wishlist.html" title="Add to Wishlist"><i
-                                                        class="fa fa-heart-o" aria-hidden="true"></i></a></li>
-                                            <li class="compare"><a href="#" title="compare"><span class="ion-levels"></span></a>
-                                            </li>
-                                            <li class="quick_button"><a href="#" data-bs-toggle="modal" data-bs-target="#modal_box"
-                                                    title="quick view"> <span class="ion-ios-search-strong"></span></a></li>
-                                        </ul>
-                                    </div>
-                                    <div class="add_to_cart">
-                                        <a href="cart.html" title="add to cart">Thêm vào giỏ hàng</a>
-                                    </div>
-                                    {{-- <div class="product_timing">
+                                <div class="action_links">
+                                    <ul>
+                                        <li class="wishlist"><a href="wishlist.html" title="Add to Wishlist"><i
+                                                    class="fa fa-heart-o" aria-hidden="true"></i></a></li>
+                                        <li class="compare"><a href="#" title="compare"><span
+                                                    class="ion-levels"></span></a>
+                                        </li>
+                                        <li class="quick_button"><a href="#" data-bs-toggle="modal"
+                                                data-bs-target="#modal_box" title="quick view"> <span
+                                                    class="ion-ios-search-strong"></span></a></li>
+                                    </ul>
+                                </div>
+                                <div class="add_to_cart">
+                                    <form action="{{route('user.cart.add')}}" method="POST" class="add-to-cart-form"
+                                        id="add-to-cart-form">
+                                        @csrf
+                                        <input type="hidden" name="MaSP" value="{{ $product->MaSP }}">
+                                        <input type="number" name="soLuong" value="1" min="1" style="display: none;">
+                                        <button type="submit" class="btn btn-primary">Thêm vào giỏ hàng</button>
+                                    </form>
+                                </div>
+                                {{-- <div class="product_timing">
                                         <div data-countdown="2023/12/15"></div>
                                     </div> --}}
+                            </div>
+                            <figcaption class="product_content">
+                                <div class="price_box">
+                                    {{-- <span class="old_price">$86.00</span> --}}
+                                    <span
+                                        class="current_price">{{ number_format($product->getProductPrice(), 0, ',', '.') }}
+                                        đ</span>
                                 </div>
-                                <figcaption class="product_content">
-                                    <div class="price_box">
-                                        {{-- <span class="old_price">$86.00</span> --}}
-                                        <span class="current_price">{{ number_format($product->getProductPrice(), 0, ',', '.') }} đ</span>
-                                    </div>
-                                    <h3 class="product_name"><a href="{{route('user.home.show')}}">{{ $product->getProductName() }}</a></h3>
-                                </figcaption>
-                            </figure>
-                        </article>
+                                <h3 class="product_name"><a
+                                        href="{{route('user.home.show')}}">{{ $product->getProductName() }}</a></h3>
+                            </figcaption>
+                        </figure>
+                    </article>
                     @endforeach
                 </div>
             </div>
