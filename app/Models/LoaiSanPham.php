41fb656274c93622ee5201570824c6c44d9d52c5
Khoa Nguyễn <0306221240@caothang.edu.vn>
2025-01-10 04:09:13 +0700

    Khoa đã thêm các file models và factories cần thiết.

---
+++ app/Models/LoaiSanPham.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class LoaiSanPham extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'MaLSP';
+
+    protected $fillable = [
+        "TenLSP",
+        "TrangThai",
+    ];
+
+    public function getCategoryId() {
+        return $this->attributes[ 'MaLSP' ];
+    }
+
+    public function setCategoryId( $id ) {
+        $this->attributes[ 'MaLSP' ] = $id;
+    }
+
+    public function getCategoryName() {
+        return $this->attributes[ 'TenLSP' ];
+    }
+
+    public function setCategoryName( $name ) {
+        $this->attributes[ 'TenLSP' ] = $name;
+    }
+
+    public function getCategoryStatus() {
+        return $this->attributes[ 'TrangThai' ];
+    }
+
+    public function setCategoryStatus( $status ) {
+        $this->attributes[ 'TrangThai' ] = $status;
+    }
+}
--- app/Models/SanPham.php
+++ app/Models/SanPham.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class SanPham extends Model
 {
+    use HasFactory;
+
     protected $table = 'san_phams';
     protected $primaryKey = 'MaSP';
     public $timestamps = false;
